9756f09f7955039f1b8f15a2b68178eedcc54293
ateliee <ateliee@gmail.com>
2025-05-29 09:45:16 +0900

    fix: レイアウト調整

--- nuxt/components/LoanResult.vue
+++ nuxt/components/LoanResult.vue
@@ -7,29 +7,31 @@
         <v-table density="compact" fixed-header height="calc(100vh - 400px)">
           <thead>
             <tr>
+              <th class="text-end">年</th>
               <th class="text-end">月</th>
               <th class="text-end font-weight-bold">ローン返済額</th>
+              <th class="text-end">ローン残金</th>
               <th class="text-end">諸経費</th>
-              <th class="text-end">月々返済額</th>
-              <th class="text-end">残元金</th>
-              <th class="text-end">貯蓄合計額</th>
+              <th class="text-end font-weight-bold">月々の支払い</th>
+              <th class="text-end font-weight-bold">貯蓄合計額</th>
             </tr>
           </thead>
           <tbody>
             <tr
               v-for="(row, idx) in resultTable"
               :key="idx"
-              :class="earlyRepaymentYears && idx === earlyRepaymentYears * 12 - 1 ? 'highlight-row' : ''"
+              :class="{'highlight-row': earlyRepaymentYears && idx === earlyRepaymentYears * 12 - 1, 'end-year-row': ((idx + 1) % 12) === 0 }"
             >
+              <td class="text-end">{{ Math.floor(idx / 12) + 1 }}年目</td>
               <td class="text-end">{{ idx + 1 }}</td>
               <td class="text-end">
                 <div class="font-weight-bold">{{ row.loanPayment.toLocaleString() }}</div>
                 <div class="text-caption">(元本: {{ row.principalPayment.toLocaleString() }} 利息: {{ row.interest.toLocaleString() }})</div>
               </td>
-              <td class="text-end">{{ row.cost.toLocaleString() }}</td>
-              <td class="text-end">{{ row.totalPayment.toLocaleString() }}</td>
               <td class="text-end">{{ row.remainingPrincipal.toLocaleString() }}</td>
-              <td class="text-end">{{ ((savings - row.totalPayment) * (idx + 1)).toLocaleString() }}</td>
+              <td class="text-end">{{ row.cost.toLocaleString() }}</td>
+              <td class="text-end font-weight-bold">{{ row.totalPayment.toLocaleString() }}</td>
+              <td class="text-end font-weight-bold">{{ ((savings - row.totalPayment) * (idx + 1)).toLocaleString() }}</td>
             </tr>
           </tbody>
         </v-table>
@@ -58,5 +60,9 @@ defineProps({
 
 .highlight-row td {
   background-color: rgba(var(--v-theme-primary), 0.1);
+}
+
+.end-year-row td {
+  border-bottom-width: 4px !important;
 }
 </style>
